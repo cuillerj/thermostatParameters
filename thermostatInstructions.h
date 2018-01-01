f6bcdd93a6b5eef64cc3de47279ac35d2e27bc88
cuillerj <jean.cuiller@wanadoo.fr>
2018-01-01 17:26:47 +0100

    tunning

--- thermostatInstructions.h
+++ thermostatInstructions.h
@@ -1,17 +1,17 @@
 
 /* default values
 */
-#define reactivity 65     // express in 1/10 of °celsius the heating system capacity to increase temperature in 30mn with 10° outside tempreature
+#define reactivity 65     // express in 1/100 of °celsius the heating system capacity to increase temperature in 30mn with 10° outside tempreature
 #define sizeAnticipation 7  // the window size of 30mn schedule steps that is taken into account to anticipate the warming need
 #define maximumTemperature 220  // 
 #define minimumTemperature 100
 #define outHomeTemperatureDecrease 5
 #define KpPID 20
 #define KiPID 30
-#define KpdPID 45
+#define KdPID 45
 #define thresholdPID 7         // 255 means no PID
-#define PIDCyleDelay 40 // expressed in mn*10 ex: 45 means 4mn30s
-#define hysteresisDelay 30 // expressed in mn*10 ex: 45 means 4mn30s
+#define PIDCyleDelay 30 // expressed in mn*10 ex: 30 means 300sec = 5mn
+#define hysteresisDelay 24 // expressed in mn*10 ex: 24 means 240 sec = 4mn
 
 #define tempListSize 5
 #define notFreezeIdx 4
